42e9b609bf55a6daf957475334d2e4a38fea7d02
mgumieniak <maciejgumieniak007@gmail.com>
2020-12-23 23:24:30 +0100

    Init commit

---
+++ src/main/java/com/web/commonrest/Test2.java
@@ -0,0 +1,9 @@
+package com.web.commonrest;
+
+import lombok.Value;
+
+@Value
+public class Test2 {
+    String id;
+    String id2;
+}
---
+++ src/test/java/com/web/commonrest/Test2Test.java
@@ -0,0 +1,12 @@
+package com.web.commonrest;
+
+import org.junit.jupiter.api.Test;
+
+
+class Test2Test {
+
+    @Test
+    public void test(){
+        new Test2("1","2");
+    }
+}
